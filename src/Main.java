a131a948dcdeedbb31bdccf6df5e7b83b2cbb5ce
nastu <nastushik92@bk.ru>
2022-10-25 16:21:58 +0300

    Initial commit6

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import Transport.Car;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        Car lada=new Car(1.6,"желтый",2015,"","","528",8,"");
-        lada.information();
-        Car audi=new Car("Audi","50 L TDL quatro","Германия", 2.4, 2020,"","","м487тт597",5,"");
-       audi.information();
-        Car bmw=new Car("BMW","Z8","Германия",1.6,2021,"","","и547оо588",6,"");
-        bmw.information();
-        Car kia=new Car("Kia","Sportage","Южная Корея", 2.4,2018,"","","784",6,"");
-        kia.information();
-        Car hunday=new Car ("Hunday","Avante","Южная Корея","оранжевый",  1.6,"","","е231ее777",4,"");
-        hunday.information();
+        Car lada=new Car("Lada","Granta",1.6f,"желтый",2015,
+                "russia","mechanic","Sedan","A001MH177",8,
+                new Car.Key(true,true),
+                new Car.Insurance(
+                        LocalDate.now().plusMonths(3),
+                        1000.0f,"123456789"));
+        System.out.println(lada.toString());
+
+
     }
 
 }
--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -1,265 +1,240 @@
 package Transport;
 
+import java.time.LocalDate;
+
 public class Car {
-    public static class Inshurance {
-        private String expiringDate;
-        private double price;
-        private int number;
+    private final String brand;
+    private final String model;
+    private float engineVolume;
+    private String color;
+    private final int manufacturingYear;
+    private final String manufactureCountry;
 
-        public Inshurance(String expiringDate, double price, int number) {
-            if (expiringDate != null && !expiringDate.isEmpty()) {
-                this.expiringDate = expiringDate;
-            } else {
-                this.expiringDate = "делать новую";
-                this.price = price;
-                this.number = number;
-            }
+    private String gearBox;
+    private final String bodyType;
+    private String regNumber;
+    private final int placesCount;
+    private boolean winterTires;
+
+
+    private final Key key;
+    private final Insurance insurance;
+
+    public Car(String brand, String model,
+               float engineVolume, String color,
+               int manufacturingYear,
+               String manufactureCountry,
+               String gearBox, String bodyType,
+               String regNumber,int placesCount,
+               Key key, Insurance insurance) {
+        this.brand = validofDefault(brand, "default");
+        this.model = validofDefault(model, "default");
+        if (manufacturingYear >= 0) {
+            this.manufacturingYear = manufacturingYear;
+        } else {
+            this.manufacturingYear = 2000;
+        }
+        this.manufactureCountry = validofDefault(manufactureCountry, "russia");
+        this.bodyType = validofDefault(bodyType, "default");
+
+        this.placesCount = Math.max(placesCount, 1);
+        this.winterTires = winterTires;
+        this.key = key;
+        this.insurance = insurance;
+        setEngineVolume(engineVolume);
+        setColor(color);
+        setGearBox(gearBox);
+        setRegNumber(regNumber);
+        setWinterTires(winterTires);
+    }
+
+
+
+    private String validofDefault(String value, String defaultValue) {
+        if (value == null || value.isBlank()) {
+            return defaultValue;
+        } else {
+            return value;
         }
     }
-    public static class Key {
-        private String remoteStartEngine;
-        private String withoutKeyAccess;
 
-        public Key(String remoteStartEngine, String withoutKeyAccess) {
-            if (remoteStartEngine != null && !remoteStartEngine.isEmpty()) {
-                this.remoteStartEngine = remoteStartEngine;
-            } else {
-                this.remoteStartEngine = "запускай вручную";
+    public void setSeasonTires() {
+        int currentMonth = LocalDate.now().getDayOfMonth();
+        this.winterTires = currentMonth <= 4 || currentMonth >= 11;
+    }
 
-            }
-            if (withoutKeyAccess != null && !withoutKeyAccess.isEmpty()) {
-                this.withoutKeyAccess = withoutKeyAccess;
-            } else {
-                this.withoutKeyAccess = "примени ключ";
-            }
+    public boolean isRegNumberValid() {
+        if (this.regNumber.length() != 9) {
+            return false;
+        }
+        char[] regNumberChars = this.regNumber.toCharArray();
+        return isNumberLetter(regNumberChars[0])
+                && isNumber(regNumberChars[1])
+                && isNumber(regNumberChars[2])
+                && isNumber(regNumberChars[3])
+                && isNumber(regNumberChars[4])
+                && isNumber(regNumberChars[5])
+                && isNumber(regNumberChars[6])
+                && isNumber(regNumberChars[7])
+                && isNumber(regNumberChars[8]);
+    }
+
+    private boolean isNumber(char symbol) {
+        return symbol >= '0' && symbol <= '9';
+    }
+
+    private boolean isNumberLetter(char symbol) {
+        String allowedSymbols = "ABEKMHOPCTYX";
+        return allowedSymbols.contains("" + symbol);
+    }
+
+
+    public void setEngineVolume(float engineVolume) {
+        this.engineVolume = engineVolume >= 0.0f ? engineVolume : 1.5f;
+    }
+
+    public void setColor(String color) {
+        if (color == null || !color.isBlank()) {
+            this.color = "white";
+        } else {
+            this.color = color;
         }
     }
 
-        private String brand;
-        private String model;
-        private String productionCountry;
-        String color;
-        double engineVolume;
-        private int productionYear;
-        String gearBox;
-        private String typeOfCarcase;
-        String number;
-        private int numberOfSeats;
-        String signSummerOrWinterTires;
-        private Key key;
-        private Inshurance insh;
-
-        public Car(String brand, String model, String productionCountry, String color, double engineVolume, int productionYear, String gearBox, String number, int numberOfSeats, String signSummerOrWinterTires, String typeOfCarcase) {
-            this.brand = brand;
-            this.model = model;
-            this.productionCountry = productionCountry;
-            this.color = color;
-            this.engineVolume = engineVolume;
-            this.productionYear = productionYear;
-            if (gearBox != null && !gearBox.isEmpty()) {
-                this.gearBox = gearBox;
-            } else {
-                this.gearBox = "Автомат";
-            }
-            if (typeOfCarcase != null && !typeOfCarcase.isEmpty()) {
-                this.typeOfCarcase = typeOfCarcase;
-            } else {
-                this.typeOfCarcase = "Пластик";
-            }
-            this.number = number;
-            this.numberOfSeats = numberOfSeats;
-            if (signSummerOrWinterTires != null && !signSummerOrWinterTires.isEmpty()) {
-                this.signSummerOrWinterTires = signSummerOrWinterTires;
-            } else {
-                this.signSummerOrWinterTires = "демисезонные";
-            }
-
-        }
-
-
-        public Car(String brand, String model, String color, String productionCountry, int productionYear, String gearBox, String typeOfCarcase, String number, int numberOfSeats, String signSummerOrWinterTires) {
-            this.brand = brand;
-            this.model = model;
-            this.productionCountry = productionCountry;
-            this.color = color;
-            engineVolume = 1.5;
-            this.productionYear = productionYear;
-            if (gearBox != null && !gearBox.isEmpty()) {
-                this.gearBox = gearBox;
-            } else {
-                this.gearBox = "Автомат";
-            }
-            if (typeOfCarcase != null && !typeOfCarcase.isEmpty()) {
-                this.typeOfCarcase = typeOfCarcase;
-            } else {
-                this.typeOfCarcase = "Пластик";
-            }
-            this.number = number;
-            this.numberOfSeats = numberOfSeats;
-            if (signSummerOrWinterTires != null && !signSummerOrWinterTires.isEmpty()) {
-                this.signSummerOrWinterTires = signSummerOrWinterTires;
-            } else {
-                this.signSummerOrWinterTires = "демисезонные";
-            }
-        }
-
-        public Car(String brand, String model, String productionCountry, double engineVolume, int productionYear, String gearBox, String typeOfCarcase, String number, int numberOfSeats, String signSummerOrWinterTires) {
-            this.brand = brand;
-            this.model = model;
-            this.productionCountry = productionCountry;
-            color = "белый";
-            this.engineVolume = engineVolume;
-            this.productionYear = productionYear;
-            if (gearBox != null && !gearBox.isEmpty()) {
-                this.gearBox = gearBox;
-            } else {
-                this.gearBox = "Автомат";
-            }
-            if (typeOfCarcase != null && !typeOfCarcase.isEmpty()) {
-                this.typeOfCarcase = typeOfCarcase;
-            } else {
-                this.typeOfCarcase = "Пластик";
-            }
-            this.number = number;
-            this.numberOfSeats = numberOfSeats;
-            if (signSummerOrWinterTires != null && !signSummerOrWinterTires.isEmpty()) {
-                this.signSummerOrWinterTires = signSummerOrWinterTires;
-            } else {
-                this.signSummerOrWinterTires = "демисезонные";
-            }
-        }
-
-        public Car(String brand, String model, String productionCountry, String color, double engineVolume, String gearBox, String typeOfCarcase, String number, int numberOfSeats, String signSummerOrWinterTires) {
-            this.brand = brand;
-            this.model = model;
-            this.productionCountry = productionCountry;
-            this.color = color;
-            this.engineVolume = engineVolume;
-            productionYear = 2000;
-            if (gearBox != null && !gearBox.isEmpty()) {
-                this.gearBox = gearBox;
-            } else {
-                this.gearBox = "Автомат";
-            }
-            if (typeOfCarcase != null && !typeOfCarcase.isEmpty()) {
-                this.typeOfCarcase = typeOfCarcase;
-            } else {
-                this.typeOfCarcase = "Пластик";
-            }
-            this.number = number;
-            this.numberOfSeats = numberOfSeats;
-            if (signSummerOrWinterTires != null && !signSummerOrWinterTires.isEmpty()) {
-                this.signSummerOrWinterTires = signSummerOrWinterTires;
-            } else {
-                this.signSummerOrWinterTires = "демисезонные";
-            }
-        }
-
-        public Car(double engineVolume, String color, int productionYear, String gearBox, String typeOfCarcase, String number, int numberOfSeats, String signSummerOrWinterTires) {
-            brand = "default";
-            model = "default";
-            productionCountry = "default";
-            this.color = color;
-            this.engineVolume = engineVolume;
-            this.productionYear = productionYear;
-            if (gearBox != null && !gearBox.isEmpty()) {
-                this.gearBox = gearBox;
-            } else {
-                this.gearBox = "Автомат";
-            }
-            if (typeOfCarcase != null && !typeOfCarcase.isEmpty()) {
-                this.typeOfCarcase = typeOfCarcase;
-            } else {
-                this.typeOfCarcase = "Пластик";
-            }
-            this.number = number;
-            this.numberOfSeats = numberOfSeats;
-            if (signSummerOrWinterTires != null && !signSummerOrWinterTires.isEmpty()) {
-                this.signSummerOrWinterTires = signSummerOrWinterTires;
-            } else {
-                this.signSummerOrWinterTires = "демисезонные";
-            }
-
-        }
-
-        public String getBrand() {
-            return brand;
-        }
-
-        public String getModel() {
-            return model;
-        }
-
-        public String getProductionCountry() {
-            return productionCountry;
-        }
-
-        public int getProductionYear() {
-            return productionYear;
-        }
-
-        public String getTypeOfCarcase() {
-            return typeOfCarcase;
-        }
-
-        public int getNumberOfSeats() {
-            return numberOfSeats;
-        }
-
-        public String getColor() {
-            return color;
-        }
-
-        public void setColor(String color) {
-            this.color = color;
-        }
-
-        public double getEngineVolume() {
-            return engineVolume;
-        }
-
-        public void setEngineVolume(double engineVolume) {
-            this.engineVolume = engineVolume;
-        }
-
-        public String getGearBox() {
-            return gearBox;
-        }
-
-        public void setGearBox(String gearBox) {
+    public void setGearBox(String gearBox) {
+        if (gearBox == null || !gearBox.isBlank()) {
+            this.gearBox = "No information";
+        } else {
             this.gearBox = gearBox;
         }
+    }
 
-        public String getNumber() {
-            return number;
+    public void setRegNumber(String regNumber) {
+        this.regNumber = validofDefault(regNumber, "default");
+    }
+
+    public void setWinterTires(boolean winterTires) {
+        this.winterTires = winterTires;
+    }
+
+    public static class Key {
+        private final boolean remoteEngineStart;
+        private final boolean keyLessAccess;
+
+        public Key(boolean remoteEngineStart, boolean keyLessAccess) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keyLessAccess = keyLessAccess;
         }
 
-        public void setNumber(String number) {
-            this.number = number;
+        public boolean isRemoteEngineStart() {
+            return remoteEngineStart;
         }
 
-        public String getSignSummerOrWinterTires() {
-            return signSummerOrWinterTires;
+        public boolean isKeyLessAccess() {
+            return keyLessAccess;
+        }
+    }
+
+    public static class Insurance {
+        private final LocalDate validUntil;
+        private final float cost;
+        private final String number;
+
+        public Insurance(LocalDate validUntil, float cost, String number) {
+            this.validUntil = validUntil != null ? validUntil : LocalDate.now().plusDays(10);
+            this.cost = Math.max(cost, 1f);
+            if (number == null || number.isBlank()) {
+                this.number = "default";
+            } else {
+                this.number = number;
+
+            }
         }
 
-        public void setSignSummerOrWinterTires(String signSummerOrWinterTires) {
-            this.signSummerOrWinterTires = signSummerOrWinterTires;
-
+        public boolean isNumberValid() {
+            return number.length() == 9;
         }
 
-        public Key getKey() {
+        public boolean isInshuranceValid() {
+            return LocalDate.now().isBefore(this.validUntil);
+        }
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getManufacturingYear() {
+        return manufacturingYear;
+    }
+
+    public String getManufactureCountry() {
+        return manufactureCountry;
+    }
+
+    public String getGearBox() {
+        return gearBox;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public int getPlacesCount() {
+        return placesCount;
+    }
+
+    public boolean isWinterTires() {
+        return winterTires;
+    }
+
+    public Key getKey() {
         return key;
-         }
-
-    public Inshurance getInsh() {
-        return insh;
     }
 
-    public void information() {
-            System.out.println("Mарка " + brand + " Модель " + model + " Страна производства " + productionCountry + " Цвет кузова " + color + " Объем двигателя в литрах " + engineVolume + " Год производства " + productionYear);
-            System.out.println(" Коробка передач " + gearBox + " Тип кузова " + typeOfCarcase + " Регистрационный номер " + number + " Количество мест " + numberOfSeats + " признак Летняя или Зимняя резина " + signSummerOrWinterTires);
-        }
+    public Insurance getInsurance() {
+        return insurance;
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engineVolume=" + engineVolume +
+                ", color='" + color + '\'' +
+                ", manufacturingYear=" + manufacturingYear +
+                ", manufactureCountry='" + manufactureCountry + '\'' +
+                ", gearBox='" + gearBox + '\'' +
+                ", bodyType='" + bodyType + '\'' +
+                ", regNumber='" + regNumber + '\'' +
+                ", placesCount=" + placesCount +
+                ", winterTires=" + winterTires +
+                ", key=" + key +
+                ", insurance=" + insurance +
+                '}';
+    }
+}
+
+
+
+
 
 
 
